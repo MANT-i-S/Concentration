a3a0276a5ce64d02b2c3cc0a510cfd0cd1b27d6f
MANT-i-S <mantis.s95@gmail.com>
2021-04-23 18:07:59 -0700

    Reimplemented var, added extension

--- Concentration/Model/Concentration.swift
+++ Concentration/Model/Concentration.swift
@@ -12,21 +12,11 @@ class Concentration {
     
     private(set) var flipCount = 0
     
-    private(set) var gameScore = 0 //TODO implement gamescore with memory of cards you've seen already.
+    private(set) var gameScore = 0
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -76,5 +66,12 @@ class Concentration {
             cards += [card, card]
         }
         cards.shuffle()
+    }
+}
+
+// Usend in indexOfOneAndOnlyFaceUpCard
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
